a0cde55f68d4a010a74607b83b8fc9729f4bf499
Ândriu Felipe Coelho <andriu_coelho1993@hotmail.com>
2021-09-14 17:19:08 -0300

    #aula1

---
+++ AluraViagens/View/Cells/Oferta/OfertaTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OfertaTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Ândriu Felipe Coelho on 23/08/21.
+//
+
+import UIKit
+
+class OfertaTableViewCell: UITableViewCell {
+    
+}
